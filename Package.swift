b8050759458d874bb017e5537d5ec40a153a64ed
Johnny Vega <johnny.vega@me.com>
2019-12-09 23:33:35 -0600

    Update Dynamic Libraries

--- Package.swift
+++ Package.swift
@@ -13,7 +13,6 @@ let package = Package (
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "NMOutlineView",
-            type: .dynamic,
             targets: ["NMOutlineView"])
     ],
     dependencies: [
